5209a31a014a0a6972d3b0eaefa0f25a192cc002
ch1bi <maine1191@gmail.com>
2017-07-22 15:45:39 -0400

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,179 @@
+
+
+angular.module('deepBlue', ['ionic', 'deepBlue.controllers', 'deepBlue.services'])
+
+.run(function($ionicPlatform, $rootScope, $timeout, $state) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  /*
+
+    Here we setup the views of our app.
+    In this case:
+    - feed, account, shop, checkout, cart will require login
+    - app will go to the "start view" when launched.
+
+    #IMPLEMENTATION-DETAIL: views that require authorizations have an
+    "auth" key with value = "true".
+
+  */
+  
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  
+  .state('app.start', {
+    url: '/start',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/start.html'
+      }
+    }
+  })
+
+  .state('app.login', {
+    url: '/login',
+    cached : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html',
+        controller : 'LoginCtrl'
+      }
+    }
+  })
+
+  .state('app.login2', {
+    url: '/login',
+    cached : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login2.html',
+        controller : 'LoginCtrl2'
+      }
+    }
+  })
+
+  .state('app.forgot', {
+    url: '/forgot',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/forgot.html'
+      }
+    }
+  })
+
+  .state('app.signup', {
+    url: '/signup',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/signup.html',
+        controller : 'AppCtrl'
+      }
+    }
+  })
+
+  .state('app.account', {
+      url: '/account',
+      data : { auth : true },
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/account.html',
+          controller : 'AccountCtrl'
+        }
+      }
+  })
+
+  .state('app.feed', {
+    url: '/feed',
+    data : { auth : true },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/feed.html',
+        controller : 'FeedsCtrl'
+      }
+    }
+  })
+
+  .state('app.storehome', {
+    url: '/storehome',
+    data : { auth : true },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/storehome.html',
+        controller : 'FeedsCtrl'
+      }
+    }
+  })
+
+  .state('app.shop', {
+    url: '/shop',
+
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/shop.html',
+        controller : 'ShopCtrl'
+      }
+    }
+  })
+
+   .state('app.sectionDetail', {
+    url: '/sections',
+
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/consumer/storeSections.html',
+        controller : 'ShopCtrl'
+      }
+    }
+  })
+
+  .state('app.cart', {
+    url: '/cart',
+    data : { auth : true },
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/cart.html',
+        controller : 'CartCtrl'
+      }
+    }
+  })
+
+  .state('app.checkout', {
+    url: '/checkout',
+    data : { auth : true },
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/checkout.html',
+        controller : 'CheckoutCtrl'
+      }
+    }
+  })
+  
+  // If none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/start');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,383 @@
+
+
+//controllers are packed into a module
+angular.module('deepBlue.controllers', ["firebase"])
+
+
+.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) { 
+    return $firebaseAuth();
+  }
+])
+
+//top view controller
+.controller('AppCtrl', function($scope, $state, $rootScope, $state, Auth,$ionicPopup,$ionicModal) {
+  
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // Simplified handling and logout function.
+  // A real app would delegate a service for organizing session data
+  // and auth stuff in a better way.
+  // $rootScope.user = {};
+
+  
+$scope.createUser = function(data){
+
+  $scope.data = {
+
+    user: data.type,
+    name: data.name,
+    email: data.email,
+    password:data.password
+  }
+
+  console.log($scope.data.user)
+    //get reference to our database
+    var database = firebase.database();
+
+         // Create a new user
+      Auth.$createUserWithEmailAndPassword($scope.data.email, $scope.data.password)
+
+        .then(function(user) {
+        //save user info here
+         database.ref('users/'+ user.uid).set({
+
+          email:$scope.data.email,
+          "firstName" :$scope.data.name,
+          "type": $scope.data.user
+    })
+
+          
+     $ionicPopup.alert({
+     title: 'Success!',
+     template: "User Created!"
+   })
+
+   .then(function(){
+
+     $state.go("app.login")
+   })
+           
+
+      })
+  
+      .catch(function(error) {
+        
+        console.log(error)
+        
+         $ionicPopup.alert({
+     title: 'Error',
+     template: error.message
+   });
+        
+          
+        });
+
+}
+
+  $scope.logout = function(){
+    $rootScope.user = {};
+    $state.go('app.start')
+  };
+
+})
+
+// This controller is bound to the "app.account" view
+.controller('AccountCtrl', function($scope, $rootScope) {
+  
+  //readonly property is used to control editability of account form
+  $scope.readonly = true;
+
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // We act on a copy of the root user
+  $scope.accountUser = angular.copy($rootScope.user);
+  var userCopy = {};
+
+  $scope.startEdit = function(){
+    $scope.readonly = false;
+    userCopy = angular.copy($scope.user);
+  };
+
+  $scope.cancelEdit = function(){
+    $scope.readonly = true;
+    $scope.user = userCopy;
+  };
+  
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // this function should call a service to update and save 
+  // the data of current user.
+  // In this case we'll just set form to readonly and copy data back to $rootScope.
+  $scope.saveEdit = function(){
+    $scope.readonly = true;
+    $rootScope.user = $scope.accountUser;
+  };
+
+})
+
+
+.controller('LoginCtrl', function ($scope, $state, $ionicPopup,$rootScope) {
+
+  $scope.doLogin = function() {
+    
+    //current signed in user
+     var userId = firebase.auth().currentUser
+
+    firebase.auth().signInWithEmailAndPassword(this.loginData.email, this.loginData.password)
+
+    .then(function(){
+
+      //Read the type of user, then send to correct UI
+
+    var userId = firebase.auth().currentUser.uid;
+  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    console.log(snapshot.val())
+
+    if(snapshot.val().type === "consumer"){
+
+      $state.go("app.feed") 
+    }
+
+    // else if(snapshot.val().type === "business"){
+
+    //     $state.go("owner.home") 
+    // }
+ 
+})
+
+
+      console.log(user)
+   
+
+    })
+    
+    
+    .catch(function(error) {
+
+      console.log(error)
+
+          $ionicPopup.alert({
+     title: 'Error',
+     template: error.message
+   });
+ 
+});
+
+  };
+  
+})
+
+.controller('LoginCtrl2', function ($scope, $state, $rootScope) {
+
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // This login function is just an example.
+  // A real one should call a service that checks the auth against some
+  // web service
+
+  $scope.login = function(){
+    //in this case we just set the user in $rootScope
+    $rootScope.user = {
+      email : "mary@ubiqtspaces.com",
+      name : "Mary Ubiquitous",
+      address : "Rue de Galvignac",
+      city : "RonnieLand",
+      zip  : "00007",
+      avatar : 'sampledata/images/avatar.jpg'
+    };
+    //finally, we route our app to the 'app.shop' view
+    $state.go('app.feed');
+  };
+  
+})
+
+
+// Feeds controller.
+.controller('FeedsCtrl', function($scope, BackendService) {
+
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // In this example feeds are loaded from a json file.
+  // (using "getFeeds" method in BackendService, see services.js)
+  // In your application you can use the same approach or load 
+  // feeds from a web service.
+  
+  $scope.doRefresh = function(){
+      BackendService.getFeeds()
+      .success(function(newItems) {
+        $scope.feeds = newItems;
+      })
+      .finally(function() {
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+  };
+
+  // Triggering the first refresh
+  $scope.doRefresh();
+
+})
+
+.controller('FeedsCtrl', function($scope, BackendService) {
+
+  // #SIMPLIFIED-IMPLEMENTATION:
+  // In this example feeds are loaded from a json file.
+  // (using "getFeeds" method in BackendService, see services.js)
+  // In your application you can use the same approach or load 
+  // feeds from a web service.
+  
+  $scope.doRefresh = function(){
+      BackendService.getFeeds()
+      .success(function(newItems) {
+        $scope.feeds = newItems;
+      })
+      .finally(function() {
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+  };
+
+  // Triggering the first refresh
+  $scope.doRefresh();
+
+})
+
+// Shop controller.
+.controller('ShopCtrl', function($scope,$state, $ionicActionSheet, BackendService, CartService, $firebaseArray) {
+
+
+	//get reference to the stores in our database
+  var storeRef = firebase.database().ref("stores")
+
+  
+  
+  // In this example feeds are loaded from a json file.
+  // (using "getProducts" method in BackendService, see services.js)
+  // In your application you can use the same approach or load 
+  // products from a web service.
+  
+  //using the CartService to load cart from localStorage
+
+   $scope.$on("$ionicView.enter", function(){
+
+
+        $scope.getStores();
+    })
+
+	
+$scope.getStores = function(){
+
+$scope.stores = $firebaseArray(storeRef)
+
+
+}
+
+    //loads the sections of the store
+
+    $scope.storeSelect = function(index){
+
+      $state.go("app.sectionDetail")
+
+var theList = $firebaseArray(storeRef)
+
+theList.$loaded(function (list) {
+
+  //gets the store info based on the index
+var storeInfo =  theList[index];
+console.log(storeInfo)
+
+var storeName = storeInfo.$id
+
+// var storeSections = Object.keys(storeInfo.sections)
+// console.log(storeSections.toString())
+
+var sectionRef = firebase.database().ref("stores/storeName/sections")
+
+ // we load the store sections
+  $scope.sections = $firebaseArray(sectionRef)
+  console.log(storeName)
+});
+      
+    }
+
+
+  $scope.cart = CartService.loadCart();
+
+  $scope.doRefresh = function(){
+      BackendService.getProducts()
+      .success(function(newItems) {
+        $scope.products = newItems;
+      })
+      .finally(function() {
+        // Stop the ion-refresher from spinning (not needed in this view)
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+  };
+
+  // private method to add a product to cart
+  var addProductToCart = function(product){
+    $scope.cart.products.push(product);
+    CartService.saveCart($scope.cart);
+  };
+
+  // method to add a product to cart via $ionicActionSheet
+  $scope.addProduct = function(product){
+    $ionicActionSheet.show({
+       buttons: [
+         { text: '<b>Add to cart</b>' }
+       ],
+       titleText: 'Buy ' + product.title,
+       cancelText: 'Cancel',
+       cancel: function() {
+          // add cancel code if needed ..
+       },
+       buttonClicked: function(index) {
+         if(index == 0){
+           addProductToCart(product);
+           return true;
+         }
+         return true;
+       }
+     });
+  };
+
+  //trigger initial refresh of products
+  $scope.doRefresh();
+
+})
+
+// controller for "app.cart" view
+.controller('CartCtrl', function($scope, CartService, $ionicListDelegate) {
+  
+  // using the CartService to load cart from localStorage
+  $scope.cart = CartService.loadCart();
+  
+  // we assign getTotal method of CartService to $scope to have it available
+  // in our template
+  $scope.getTotal = CartService.getTotal;
+
+  // removes product from cart (making in persistent)
+  $scope.dropProduct = function($index){
+    $scope.cart.products.splice($index, 1);
+    CartService.saveCart($scope.cart);
+    // as this method is triggered in an <ion-option-button> 
+    // we close the list after that (not strictly needed)
+    $ionicListDelegate.closeOptionButtons();
+
+  }
+})
+
+.controller('CheckoutCtrl', function($scope, CartService, $state) {
+  
+  //using the CartService to load cart from localStorage
+  $scope.cart = CartService.loadCart();
+  $scope.getTotal = CartService.getTotal;
+
+  $scope.getTotal = CartService.getTotal;
+
+  // #NOT-IMPLEMENTED: This method is just calling alert()
+  // you should implement this method to connect an ecommerce
+  // after that the cart is reset and user is redirected to shop
+  $scope.checkout = function(){
+    alert("this implementation is up to you!");
+    $scope.cart = CartService.resetCart();
+    $state.go('app.shop')
+  }
+
+})
